a684dba888783c5dccec6f7ec38a961ce600277a
Aniket Savant <Aniket.Savant@extentia05.local>
2019-02-06 18:07:50 +0530

    designed notification screen

--- src/app/notification/notification.page.ts
+++ src/app/notification/notification.page.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificationPage implements OnInit {
 
+  slideOpts = {
+    effect: 'flip'
+  };
+  public isToggled: boolean;
+
+
   constructor() { }
 
   ngOnInit() {
+    this.isToggled = true;
   }
 
 }
